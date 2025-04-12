b260d0a09dec0ac0d25bb1d67f7fda7fd19e3776
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-12 15:14:58 -0500

    feat: delete unused routes

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('layout');
 });
 
-//Parameters using Route
-Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname) {
-    return $firstname . " " . $lastname;
-});
 
-//Named routes
-Route::get('/test', function () {
-    return "This is a test!";
-})->name("testPage");
