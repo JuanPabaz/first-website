37097ead94dd7ec3d54036d6b237a1adafc9a438
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-22 09:26:16 -0500

    feat: start service page with the service prelude

---
+++ resources/views/services.blade.php
@@ -0,0 +1,18 @@
+@vite("resources/css/services.css")
+@extends('layout')
+@section('content')
+<div class="services-container">
+    <div class="services-prelude">
+        <div class="service-prelude-title">
+            <p >Servicios</p>
+            <h1>Inicia tu sueño</h1>
+        </div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Eos, veniam. Cumque quam deserunt beatae facere laborum 
+            magni animi similique sit reprehenderit nihil, suscipit 
+            itaque repudiandae ab adipisci repellendus fugiat dolor?
+        </p>
+    </div>
+</div>
+@endsection
