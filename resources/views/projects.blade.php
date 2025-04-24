a025badfad755939edfe787c58dfe6350ebdf5d2
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-24 13:58:15 -0500

    feat: add paginator to project list

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -50,6 +50,98 @@
                 dignissimos temporibus facilis fugit veniam adipisci architecto?
             </p>
         </div>
+        <div class="project-details">
+            <img src="{{ asset('images/proyecto1.png') }}" alt="">
+            <p class="project-date">20 Ene 2024</p>
+            <h4 class="project-title">Alto de palmas</h4>
+            <p class="project-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Voluptates ipsa asperiores, illo nemo, culpa illum nam, 
+                maxime expedita quos nesciunt enim unde voluptatibus 
+                dignissimos temporibus facilis fugit veniam adipisci architecto?
+            </p>
+        </div>
+        <div class="project-details">
+            <img src="{{ asset('images/proyecto2.png') }}" alt="">
+            <p class="project-date">19 Ene 2024</p>
+            <h4 class="project-title">Cuarzo Tierra Firme</h4>
+            <p class="project-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Voluptates ipsa asperiores, illo nemo, culpa illum nam, 
+                maxime expedita quos nesciunt enim unde voluptatibus 
+                dignissimos temporibus facilis fugit veniam adipisci architecto?
+            </p>
+        </div>
+        <div class="project-details">
+            <img src="{{ asset('images/proyecto3.png') }}" alt="">
+            <p class="project-date">18 Ene 2024</p>
+            <h4 class="project-title">Burbuja Joy Bakery</h4>
+            <p class="project-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Voluptates ipsa asperiores, illo nemo, culpa illum nam, 
+                maxime expedita quos nesciunt enim unde voluptatibus 
+                dignissimos temporibus facilis fugit veniam adipisci architecto?
+            </p>
+        </div>
+        <div class="project-details">
+            <img src="{{ asset('images/proyecto1.png') }}" alt="">
+            <p class="project-date">20 Ene 2024</p>
+            <h4 class="project-title">Alto de palmas</h4>
+            <p class="project-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Voluptates ipsa asperiores, illo nemo, culpa illum nam, 
+                maxime expedita quos nesciunt enim unde voluptatibus 
+                dignissimos temporibus facilis fugit veniam adipisci architecto?
+            </p>
+        </div>
+        <div class="project-details">
+            <img src="{{ asset('images/proyecto2.png') }}" alt="">
+            <p class="project-date">19 Ene 2024</p>
+            <h4 class="project-title">Cuarzo Tierra Firme</h4>
+            <p class="project-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Voluptates ipsa asperiores, illo nemo, culpa illum nam, 
+                maxime expedita quos nesciunt enim unde voluptatibus 
+                dignissimos temporibus facilis fugit veniam adipisci architecto?
+            </p>
+        </div>
+        <div class="project-details">
+            <img src="{{ asset('images/proyecto3.png') }}" alt="">
+            <p class="project-date">18 Ene 2024</p>
+            <h4 class="project-title">Burbuja Joy Bakery</h4>
+            <p class="project-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Voluptates ipsa asperiores, illo nemo, culpa illum nam, 
+                maxime expedita quos nesciunt enim unde voluptatibus 
+                dignissimos temporibus facilis fugit veniam adipisci architecto?
+            </p>
+        </div>
+    </div>
+    <hr style="margin: 0 9rem; background-color: #EAECF0">    
+    <div class="paginator">
+        <div>
+            Anterior
+        </div>
+        <div class="pages">
+            <div class="page-number">
+                1
+            </div>
+            <div class="page-number">
+                2
+            </div>
+            <div class="page-number">
+                3
+            </div>
+            <div class="page-number">
+                4
+            </div>
+            <div class="page-number">
+                5
+            </div>
+        </div>
+        <div>
+            Siguiente
+        </div>
     </div>
 </div>
 @endsection
