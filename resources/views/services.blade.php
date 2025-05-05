48e90c23b2679573d02eb28acf87aeb11fa6abbe
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-05-05 14:47:14 -0500

    feat: refactor service list

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -15,68 +15,80 @@
         </p>
     </div>
     <div class="services">
-        <div class="service">
-            <div class="service-content">
-                <h2 class="service-title">Diseño interior</h2>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                    Voluptatum rem nulla eaque corporis temporibus quisquam 
-                </p>
-                <div class="service-buttons">
-                    <button class="know-more-button">Conoce más</button>
-                    <button class="cotizar-service-button">Cotizar</button>
-                </div>
-            </div>
-            <div class="service-img">
-                <img src="{{ asset('images/service-img.jpg') }}" alt="">
-            </div>
+        <div class="service-titles">
+            <p class="service-titles-subtitle">Inicia tu sueño</p>
+            <h2>Nuestros servicios</h2>
+            <p class="service-titles-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Eos, veniam. Cumque quam deserunt beatae facere laborum 
+                magni animi similique sit reprehenderit nihil, suscipit 
+                itaque repudiandae ab adipisci repellendus fugiat dolor?
+            </p>
         </div>
-        <div class="service">
-            <div class="service-content">
-                <h2 class="service-title">Diseño comercial</h2>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                    Voluptatum rem nulla eaque corporis temporibus quisquam 
-                </p>
-                <div class="service-buttons">
-                    <button class="know-more-button">Conoce más</button>
-                    <button class="cotizar-service-button">Cotizar</button>
+        <div class="service-list">
+            <div class="service">
+                <div class="service-content">
+                    <hr class="yellow-line">
+                    <h2 class="service-title">Diseño interior</h2>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Voluptatum rem nulla eaque corporis temporibus quisquam 
+                    </p>
+                    <div class="service-prices">
+                        <i class="fa-solid fa-location-dot" style="color: #FFFFFF"></i>
+                        <p>$340.000 COP m2</p>
+                        <i class="fa-regular fa-clock" style="color: #FFFFFF"></i>
+                        <p>1 semana</p>
+                    </div>
                 </div>
             </div>
-            <div class="service-img">
-                <img src="{{ asset('images/service-img.jpg') }}" alt="">
-            </div>
-        </div>
-        <div class="service">
-            <div class="service-content">
-                <h2 class="service-title">Diseño arquitectónico</h2>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                    Voluptatum rem nulla eaque corporis temporibus quisquam 
-                </p>
-                <div class="service-buttons">
-                    <button class="know-more-button">Conoce más</button>
-                    <button class="cotizar-service-button">Cotizar</button>
+            <div class="service">
+                <div class="service-content">
+                    <hr class="yellow-line">
+                    <h2 class="service-title">Diseño comercial</h2>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Voluptatum rem nulla eaque corporis temporibus quisquam 
+                    </p>
+                    <div class="service-prices">
+                        <i class="fa-solid fa-location-dot" style="color: #FFFFFF"></i>
+                        <p>$340.000 COP m2</p>
+                        <i class="fa-regular fa-clock" style="color: #FFFFFF"></i>
+                        <p>1 semana</p>
+                    </div>
                 </div>
             </div>
-            <div class="service-img">
-                <img src="{{ asset('images/service-img.jpg') }}" alt="">
-            </div>
-        </div>
-        <div class="service">
-            <div class="service-content">
-                <h2 class="service-title">Renderización y modelado 3D</h2>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                    Voluptatum rem nulla eaque corporis temporibus quisquam 
-                </p>
-                <div class="service-buttons">
-                    <button class="know-more-button">Conoce más</button>
-                    <button class="cotizar-service-button">Cotizar</button>
+            <div class="service">
+                <div class="service-content">
+                    <hr class="yellow-line">
+                    <h2 class="service-title">Diseño arquitectónico</h2>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Voluptatum rem nulla eaque corporis temporibus quisquam 
+                    </p>
+                    <div class="service-prices">
+                        <i class="fa-solid fa-location-dot" style="color: #FFFFFF"></i>
+                        <p>$340.000 COP m2</p>
+                        <i class="fa-regular fa-clock" style="color: #FFFFFF"></i>
+                        <p>1 semana</p>
+                    </div>
                 </div>
             </div>
-            <div class="service-img">
-                <img src="{{ asset('images/service-img.jpg') }}" alt="">
+            <div class="service">
+                <div class="service-content">
+                    <hr class="yellow-line">
+                    <h2 class="service-title">Renderización y modelado 3D</h2>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Voluptatum rem nulla eaque corporis temporibus quisquam 
+                    </p>
+                    <div class="service-prices">
+                        <i class="fa-solid fa-location-dot" style="color: #FFFFFF"></i>
+                        <p>$340.000 COP m2</p>
+                        <i class="fa-regular fa-clock" style="color: #FFFFFF"></i>
+                        <p>1 semana</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
