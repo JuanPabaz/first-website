283eeb7878d4dc15fda47093ec91536614735a5b
JuanPabaz <jpgthf@gmail.com>
2025-07-23 10:54:35 -0500

    feat: add changes

--- resources/views/project-view.blade.php
+++ resources/views/project-view.blade.php
@@ -5,6 +5,14 @@
 @section('content')
 <div class="project-detail-container">
     <div class="project-header">
+        <div class="back-button-container">
+            <a href="{{ route('proyectos') }}" class="back-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+                Proyectos
+            </a>
+        </div>
         <h1>{{ $project->name }}</h1>
         <p class="project-subtitle">{{ $project->subtitulo }}</p>
         <p class="vuotta">Vuotta Arquitectura</p>
@@ -44,7 +52,7 @@
     @endif
 
     <div class="project-description">
-        <p>{{ $project->description }}</p>
+        <p class="description-text">{{ $project->description }}</p>
     </div>
 </div>
 @endsection
