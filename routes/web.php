62322b12d1b0971cd5c3b9704cd5a668f8ec3f6f
JuanPabaz <jpgthf@gmail.com>
2026-03-19 18:36:15 -0500

    feat: add route for admin panel

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 
-Route::redirect('/', '/home');
+Route::domain('vuottaarq.com')->group(function () {
+    Route::redirect('/', '/home');
+    Route::get('/home', [ProjectController::class, 'index'])->name("inicio");
+    Route::get('/services', function () {
+        return view('services');
+    })->name("servicios");
+    Route::get('/us', function () {
+        return view('us');
+    })->name("somos");
+    Route::get('/projects', [ProjectController::class, 'list'])->name("proyectos");
+    Route::get('/projects/{project}', [ProjectController::class, 'show'])->name("proyectos.detalle");
+});
 
-Route::get('/home', [ProjectController::class, 'index'])->name("inicio");
-
-Route::get('/services', function () {
-    return view('services');
-})->name("servicios");
-
-Route::get('/us', function () {
-    return view('us');
-})->name("somos");
-
-Route::get('/projects', [ProjectController::class, 'list'])->name("proyectos");
-
-Route::get('/projects/{project}', [ProjectController::class, 'show'])->name("proyectos.detalle");
-
-
+// Rutas para el SUBDOMINIO (admin.vuottaarq.com)
+Route::domain('admin.vuottaarq.com')->group(function () {
+    Route::get('/', function () {
+        return view('admin');
+    });
+});
